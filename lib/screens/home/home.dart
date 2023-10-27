05466edb02f16fa60c5b2927721bc028c791fe2a
tauqeerkhattak <tauqeer745@outlook.com>
2023-10-27 13:21:11 +0200

    update - half filled text added

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -48,6 +48,7 @@ class _HomeState extends State<Home> {
           } else {
             log('UPP');
           }
+          return true;
         },
         child: PageView.builder(
           controller: _controller,
